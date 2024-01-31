9f1892ef6a23c706722ee5b0db325c029b491d69
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-31 20:32:14 +0100

    really complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // const [step, setStep] = useState(1);
-  // const [count, setCount] = useState(0);
-  const [range, setRange] = useState(0);
+  const [range, setRange] = useState(1);
   const [input, setInput] = useState(0);
 
   const date = new Date();
   date.setDate(date.getDate() + input);
+
+  function reset() {
+    setRange(1);
+    setInput(0);
+  }
 
   return (
     <div className="App">
@@ -19,24 +22,17 @@ function App() {
           min={0}
           max={10}
           value={range}
-          onChange={(e) => setRange(e.target.value)}
-        />{" "}
+          onChange={(e) => setRange(Number(e.target.value))}
+        />
         {range}
-        {/* <button onClick={() => setStep((s) => s - 1)}>-</button>
-        Step: {step}
-        <button onClick={() => setStep((s) => s + 1)}>+</button> */}
       </div>
       <div>
-        <button onClick={() => setInput((i) => i - Number(range))}>-</button>
+        <button onClick={() => setInput((i) => i - range)}>-</button>
         <input
           value={input}
           onChange={(e) => setInput(Number(e.target.value))}
         />
-        <button onClick={() => setInput((i) => i + Number(range))}>+</button>
-
-        {/* <button onClick={() => setCount((c) => c - step)}>-</button>
-        Count: {count}
-        <button onClick={() => setCount((c) => c + step)}>+</button> */}
+        <button onClick={() => setInput((i) => i + range)}>+</button>
       </div>
       <span>
         {input === 0
@@ -46,6 +42,11 @@ function App() {
           : input + " days from now is "}
       </span>
       <span>{date.toDateString()}</span>
+      {(input !== 0 || range !== 1) && (
+        <div>
+          <button onClick={reset}>RESET</button>
+        </div>
+      )}
     </div>
   );
 }

3da8fd3e9298cb7ea6d7b9ba817f90d49237809e
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-31 19:33:08 +0100

    got the assignment right

--- src/App.js
+++ src/App.js
@@ -3,30 +3,47 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  const [range, setRange] = useState(0);
+  const [input, setInput] = useState(0);
 
   const date = new Date();
-  date.setDate(date.getDate() + count);
+  date.setDate(date.getDate() + input);
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={range}
+          onChange={(e) => setRange(e.target.value)}
+        />{" "}
+        {range}
+        {/* <button onClick={() => setStep((s) => s - 1)}>-</button>
         Step: {step}
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <button onClick={() => setStep((s) => s + 1)}>+</button> */}
       </div>
       <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <button onClick={() => setInput((i) => i - 1)}>-</button>
+        <input
+          value={input}
+          onChange={(e) => setInput(Number(e.target.value))}
+        />
+        <button onClick={() => setInput((i) => i + 1)}>+</button>
+
+        {/* <button onClick={() => setCount((c) => c - step)}>-</button>
         Count: {count}
-        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button> */}
       </div>
       <span>
-        {count === 0
+        {input === 0
           ? "Today is "
-          : count < 0
-          ? count + " days ago was"
-          : count + " days from now is "}
+          : input < 0
+          ? input + " days ago was"
+          : input + " days from now is "}
       </span>
       <span>{date.toDateString()}</span>
     </div>

26a99a561fd9ecac335974c9305eb9c794634bf3
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-31 18:31:47 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        Step: {step}
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        Count: {count}
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <span>
+        {count === 0
+          ? "Today is "
+          : count < 0
+          ? count + " days ago was"
+          : count + " days from now is "}
+      </span>
+      <span>{date.toDateString()}</span>
     </div>
   );
 }
